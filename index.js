33d9c978e74260426173adcf79ebf50bb2286998
luis vilches <lvilches21@gmail.com>
2020-09-03 13:36:26 -0400

    cors

--- index.js
+++ index.js
@@ -13,7 +13,26 @@ const settings = require('./settings');
 const { db } = settings;
 exports.settings = settings;
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
+
+
+
 // promesa formidable
 const promiseForm = (req) => {
     const form = formidable({ multiples: true });
