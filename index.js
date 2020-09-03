55779f37d908cc0e5019049231e29c779977bef9
luis vilches <lvilches21@gmail.com>
2020-09-03 13:41:52 -0400

    cors

--- index.js
+++ index.js
@@ -13,24 +13,19 @@ const settings = require('./settings');
 const { db } = settings;
 exports.settings = settings;
 
-// app.use(cors());
+var allowlist = ['http://167.99.168.155', 'http://121.0.0.1','http://localhost:8080','http://localhost:8081']
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
-
-
+app.use(cors(corsOptionsDelegate));
 
 
 // promesa formidable
