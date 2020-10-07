a241ee19a4dbb82227bf4ecec1aa0cbb72745ccc
luis vilches <lvilches21@gmail.com>
2020-10-07 00:42:22 -0300

    flow

--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@ const env = process.env.ENVIRONMENT || 'development';
 
 module.exports = {
     db: env === 'production' ? process.env.DB_PRODUCTION : process.env.DB_DEVELOPMENT,
-    site: "https://inspector-hogar.herokuapp.com"
+    site: "https://ih-socialventis.netlify.app"
 }
