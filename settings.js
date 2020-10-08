fa9fb6e933a66a81cf5027690b89ca4ec3f96286
luis vilches <lvilches21@gmail.com>
2020-10-08 07:07:55 -0300

    transbank

--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@ const env = process.env.ENVIRONMENT || 'development';
 
 module.exports = {
     db: env === 'production' ? process.env.DB_PRODUCTION : process.env.DB_DEVELOPMENT,
-    site: "https://ih-socialventis.netlify.app"
+    site: "https://inspector-hogar.herokuapp.com" //"https://ih-socialventis.netlify.app"
 }
