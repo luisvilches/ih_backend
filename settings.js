e2846c43ec54cd1bf47e9236952fe301a9d96cef
luis vilches <lvilches21@gmail.com>
2020-10-10 19:17:56 -0300

    run server

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@ const env = process.env.ENVIRONMENT || 'development';
 
 module.exports = {
     db: env === 'production' ? process.env.DB_PRODUCTION : process.env.DB_DEVELOPMENT,
-    site: "https://inspector-hogar.herokuapp.com",
-    public_site: "https://ih-socialventis.netlify.app"
+    site: "https://api-ih.socialventisdev.cl",
+    public_site: "https://ih-front.socialventisdev.cl/"
 }
